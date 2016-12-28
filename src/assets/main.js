1fa9ea069a64e58c3970b9f1808751284a8bb1dc
alain martel <alainmartel@mbp-de-alain.home>
2016-12-28 19:10:56 +0100

    project done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/alainmartel.json',
+    dataType: 'jsonp',
+    success:function(response){
+      var div_badges=$('#badges');
+      var completed=response.courses.completed;
+      for(var complete in completed){
+        div_badges.append('<div class="course"></div>');
+        $('.course:last').append('<h3>'+completed[complete].title+'</h3>');
+        $('.course:last').append('<img src="'+completed[complete].badge+'"/>');
+        $('.course:last').append('<a href="'+completed[complete].url+'" class="btn btn-primary" target="_blank">See Course</a>');
+      }
+    }
+  });
 
 });
